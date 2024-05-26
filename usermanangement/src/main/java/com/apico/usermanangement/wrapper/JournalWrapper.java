68040ba30b3ef619932bbe18670fcfdd043e7533
Ashish Amar <ashishamar1999@gmail.com>
2024-05-26 06:48:11 +0530

    Level 1: Made some more config changes as per coding standards for docker compose

--- usermanangement/src/main/java/com/apico/usermanangement/wrapper/JournalWrapper.java
+++ usermanangement/src/main/java/com/apico/usermanangement/wrapper/JournalWrapper.java
@@ -1,7 +1,9 @@
 package com.apico.usermanangement.wrapper;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import static com.apico.usermanangement.constants.UserManagementConstants.JOURNAL_TYPE;
@@ -9,6 +11,8 @@ import static com.apico.usermanangement.constants.UserManagementConstants.JOURNA
 @Slf4j
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JournalWrapper {
     private String userId;
     private String message;
